54bc69db36fdd42e4175dfe76585937334ff559e
padro <padro@debianalbert.telefonica.net>
2018-10-01 16:22:20 +0200

    New test methods

--- lab/junithelloworld/src/test/java/edu/udg/caes/ExercicesIntroTestingTest.java
+++ lab/junithelloworld/src/test/java/edu/udg/caes/ExercicesIntroTestingTest.java
@@ -29,4 +29,31 @@ public class ExercicesIntroTestingTest {
         final int result = ExercicesIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
         assertEquals(result, 3);
     }
+
+
+
+
+    @Test
+    public void new_testFindLast(){
+        final int result = ExercicesIntroTesting.findLast(new int []{1, 2, 5}, 2);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void new_testLastZero(){
+        final int result = ExercicesIntroTesting.lastZero(new int []{1, 1, 1});
+        assertEquals(result, -1);
+    }
+
+    @Test
+    public void new_testCountPositive(){
+        final int result = ExercicesIntroTesting.countPositive(new int []{-4, 2, 2, 2});
+        assertEquals(result, 3);
+    }
+
+    @Test
+    public void new_testOddOrPos(){
+        final int result = ExercicesIntroTesting.oddOrPos(new int []{-2, -2, 1, 1, 4});
+        assertEquals(result, 3);
+    }
 }
